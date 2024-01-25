09c0caecfeb87d4e93b6f7ade5b29f32e8e85241
KRATIK_BOHRA <kratikbohra5@gmail.com>
2024-01-25 18:05:08 +0530

    reuable component ready , uploaded it as package on NPM , tested running properly

--- src/index.js
+++ src/index.js
@@ -1,41 +1,54 @@
+//
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const bookRoutes = require("./routes/bookRoutes");
 const Counter = require("./models/counter");
-const app = express();
-const port = 3000;
 
-// app.use(bodyParser.json());
-app.use(express.json());
+class backendMocker {
+  constructor(mongoConnectionString) {
+    this.app = express();
+    this.port = 3000;
 
-mongoose.connect("mongodb://localhost:27017/mockapi", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-mongoose.connection.once("open", () => {
-  console.log("Connected to MongoDB");
-});
+    // Use express.json() instead of bodyParser.json()
+    this.app.use(express.json());
 
-// initialising counter
-async function initializeCounter() {
-  // Check if the counter exists, if not, create it
-  const counter = await Counter.findOne({ _id: "bookId" });
+    // Connect to MongoDB with the provided connection string
+    mongoose.connect(mongoConnectionString, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    mongoose.connection.once("open", () => {
+      console.log("Connected to MongoDB");
+    });
 
-  if (!counter) {
-    await Counter.create({ _id: "bookId", sequence_value: 0 });
+    // Initialize counter
+    this.initializeCounter();
+
+    // Log requests to the console
+    this.app.use((req, res, next) => {
+      console.log(`${req.method} ${req.url}`);
+      next();
+    });
+
+    // Use bookRoutes
+    this.app.use(bookRoutes);
+  }
+
+  initializeCounter() {
+    Counter.findOne({ _id: "bookId" }).then((counter) => {
+      if (!counter) {
+        return Counter.create({ _id: "bookId", sequence_value: 0 });
+      }
+    });
+  }
+
+  startServer() {
+    this.app.listen(this.port, () => {
+      console.log(`Server is running on port ${this.port}`);
+    });
   }
 }
 
-initializeCounter();
-
-// Log requests to the console
-app.use((req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-});
-
-app.use(bookRoutes);
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+module.exports = backendMocker;
