ed7e84635b8484389179331a78ce25088a1d8048
KRATIK_BOHRA <kratikbohra5@gmail.com>
2024-01-28 22:45:07 +0530

    implement custom schema for users , therefore a fully nocode backend ready for users

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const bookRoutes = require("./routes/bookRoutes");
 const Counter = require("./models/counter");
 
 class backendMocker {
-  constructor(mongoConnectionString) {
+  constructor(mongoConnectionString, customSchema) {
     this.app = express();
     this.port = 3000;
 
@@ -31,9 +31,11 @@ class backendMocker {
       console.log(`${req.method} ${req.url}`);
       next();
     });
-    this.app.use(express.json());
+    const Book = customSchema
+      ? mongoose.model("Book", customSchema)
+      : require("./models/book");
     // Use bookRoutes
-    this.app.use(bookRoutes);
+    this.app.use(bookRoutes(Book));
   }
 
   initializeCounter() {
@@ -52,68 +54,3 @@ class backendMocker {
 }
 
 module.exports = backendMocker;
-
-// const express = require("express");
-// const bodyParser = require("body-parser");
-// const mongoose = require("mongoose");
-// const bookRoutes = require("./routes/bookRoutes");
-// const Counter = require("./models/counter");
-
-// class backendMocker {
-//   constructor(mongoConnectionString, customSchema) {
-//     this.app = express();
-//     this.port = 3000;
-
-//     // Use express.json() instead of bodyParser.json()
-//     this.app.use(express.json());
-
-//     // Connect to MongoDB with the provided connection string
-//     mongoose.connect(mongoConnectionString, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     });
-//     mongoose.connection.once("open", () => {
-//       console.log("Connected to MongoDB");
-//     });
-
-//     // Initialize counter
-//     this.initializeCounter(customSchema);
-
-//     // Log requests to the console
-//     this.app.use((req, res, next) => {
-//       console.log(`${req.method} ${req.url}`);
-//       next();
-//     });
-//     // this.app.use(express.json());
-//     this.app.use(express.raw({ type: "application/json" }));
-
-//     // Use bookRoutes
-//     this.app.use(bookRoutes);
-//   }
-
-//   initializeCounter(customSchema) {
-//     Counter.findOne({ _id: "bookId" }).then((counter) => {
-//       if (!counter) {
-//         const bookSchema = new mongoose.Schema(customSchema, {
-//           versionKey: false, // Disable the __v field
-//           // toJSON: { virtuals: true, getters: true, setters: true },
-//           // id: false, // Exclude the _id field
-//         });
-
-//         this.Book = mongoose.model("Book", bookSchema);
-//         // Counter.create({ _id: "bookId", sequence_value: 0 });
-//         // = mongoose.model("Book", bookSchema);
-//         Counter.create({ _id: "bookId", sequence_value: 1 });
-//       }
-//     });
-//   }
-
-//   startServer() {
-//     this.app.listen(this.port, () => {
-//       console.log(`Server is running on port ${this.port}`);
-//     });
-//   }
-// }
-
-// module.exports = backendMocker;
-// // module.exports = Book;
---
+++ src/routes/bookRoutes2.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const router = express.Router();
+const Book = require("../models/book");
+const Counter = require("../models/counter");
+
+router.post("/books/new", async (req, res) => {
+  try {
+    // console.log("Request Body:", req.body);
+    // autoincrement id in mongodb
+    const counter = await Counter.findOneAndUpdate(
+      { _id: "bookId" },
+      { $inc: { sequence_value: 1 } },
+      { new: true }
+    );
+    const nextBookId = counter.sequence_value;
+    //
+    // const { title, author, publishedDate } = req.body;
+    // const formattedDate = new Date(publishedDate).toISOString().split("T")[0];
+    // console.log("Request Body:", req);
+    // const book = new Book(req.body);
+    // const book = new Book({
+    //   _id: nextBookId, // Use the auto-incremented ID
+    //   title,
+    //   author,
+    //   publishedDate: formattedDate,
+    // });
+    // await book.save();
+    const book = new Book({ _id: nextBookId, ...req.body });
+    await book.save();
+
+    res.status(201).send(book);
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ error: error.message || "Bad Request" });
+  }
+});
+
+// get books
+// Read
+router.get("/books", async (req, res) => {
+  try {
+    const books = await Book.find();
+    res.status(200).send(books);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error: "Internal Server Error" });
+  }
+});
+
+// Update
+router.patch("/books/:id", async (req, res) => {
+  try {
+    const book = await Book.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.status(200).send(book);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+// Delete
+router.delete("/books/:id", async (req, res) => {
+  try {
+    const book = await Book.findByIdAndDelete(req.params.id);
+    if (!book) {
+      return res.status(404).json({ error: "Book not found" });
+    }
+
+    res.status(200).send(book);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+// };
+
+module.exports = router;
